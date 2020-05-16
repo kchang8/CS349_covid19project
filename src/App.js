5fe21ba1a662032405035201e836a309aecd0dda
santransisco <santransisco@yahoo.com>
2020-05-16 04:09:08 -0700

    Charts will work nicely now
    Only thing lef to do is the map

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
 import React from 'react';
-import { Cards, Chart, CountryPicker } from './components';
+import { Cards, Chart, CountryPicker , CovidMap} from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
 
 class App extends React.Component {
     state = {
-        data: {},
+        Totaldata:{},
+        Linedata:[],
         country: '',
+        chart:"Line"
     }
 
     async componentDidMount() {
-        const fetchedData = await fetchData();
-
-        this.setState( { data: fetchedData });
+        const fetchedData = await fetchData('',"Bar");
+        const lineData = await fetchData('',"Line");
+        this.setState( {Totaldata:fetchedData, Linedata:lineData});
     }
 
     handleCountryChange = async (country) => {
-        const fetchedData = await fetchData(country);
+        const{chart} = this.state
+        const lineData = await fetchData(country,"Line");
+        console.log("In the country change",lineData)
+        const totalData = await fetchData(country, "Bar");
+        this.setState({Totaldata:totalData, Linedata:lineData, country: country, chart:chart });
 
-        this.setState({ data: fetchedData, country: country });
+    }
+
+    handleChartChange = async (chart) => {
+        const{country} = this.state
+        const lineData = await fetchData('country',"Line");
+        const totalData = await fetchData(country, "Bar");
+        this.setState({Totaldata:totalData, Linedata:lineData, chart:chart });
 
     }
 
     render() {
-        const { data, country } = this.state;
+        const { Totaldata ,Linedata, country, chart } = this.state;
+        console.log("Chart state", chart)
+        console.log("This is dataTotal", Totaldata);
+        console.log("This is line data", Linedata);
 
         return (
             <div className={styles.container}>
                 <h1>COVID-19 Charts</h1>
-                <Cards data={data} />
-                <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Chart data={data} country={country} />
+                <div className={styles.container}>
+                    <CovidMap/>
+                </div>
+                <Cards Totaldata={Totaldata} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} handleChartChange={this.handleChartChange} />
+                <Chart data={Totaldata} LineData = {Linedata} country={country} chart = {chart} />
             </div>
         );
     }
--- src/api/index.js
+++ src/api/index.js
@@ -1,41 +1,79 @@
 import axios from 'axios';
 
-const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async (country) => {
-    let changeableUrl = url;
-
-    if(country) {
-        changeableUrl = `${url}/countries/${country}`;
+export const fetchData = async (country, chart) => {
+    if(chart ==="Bar"){
+        console.log("we fetching bar data BOII")
+        return fetchBarData(country);
     }
-
-    try {
-        const { data: { confirmed, recovered, deaths, lastUpdate }} = await axios.get(changeableUrl);
-
-        return { confirmed, recovered, deaths, lastUpdate };
-
-    } catch (error) {
-        console.log(error);
+    else{
+        return fetchDailyData(country);
     }
 }
 
-export const fetchDailyData = async () => {
+export const fetchBarData = async(country) =>{
+    //let changeableUrl = url;
+    let changeableUrl = "";
+    console.log("country",country)
+
+    if(country) {
+        changeableUrl = `https://corona.lmao.ninja/v2/countries/${country}`;
+    }
+    else{
+        changeableUrl = 'https://corona.lmao.ninja/v2/all';
+    }
+    
     try {
-        const { data } = await axios.get(`${url}/daily`);
-
-        const modifiedData = data.map((dailyData) => ({
-            confirmed: dailyData.confirmed.total,
-            deaths: dailyData.deaths.total,
-            date: dailyData.reportDate,
-        }));
-
-        return modifiedData;
+        const { data: { cases:confirmed, recovered, deaths, updated }} = await axios.get(changeableUrl);
+        var lastUpdate = new Date(updated);
+        return { confirmed, recovered, deaths, lastUpdate };
+    } catch (error) {
+        console.log(error);
+        console.log("we here");
+    }
+}
+export const fetchDailyData = async (country) => {
+    let tempUrl = ''
+    if(country){
+        console.log("we switched countries", country);
+        tempUrl = `https://corona.lmao.ninja/v2/historical/${country}?lastdays=100`;
+        console.log("Here is the tempURL", tempUrl);
+    }
+    else{
+        tempUrl = 'https://corona.lmao.ninja/v2/historical/all?lastdays=100';
+    }
+    try { 
+        const { data:data2 } = await axios.get(tempUrl);
+        console.log("Country in teh resonse:",data2.country);
+        var modifiedData2 = [];
+        var dates;
+        if(country){
+            for( dates in data2.timeline.cases){
+                modifiedData2.push({
+                    confirmed: data2.timeline.cases[dates],
+                    deaths: data2.timeline.deaths[dates],
+                    date: dates
+                })
+            }
+        }
+        else{
+            for( dates in data2.cases){
+                modifiedData2.push({
+                    confirmed: data2.cases[dates],
+                    deaths: data2.deaths[dates],
+                    date: dates
+                })
+            }
+        }
+        console.log("here is the data inside of the api call",modifiedData2);
+        return modifiedData2;
     } catch (error) {
 
     }
 }
 
 export const fetchCountries = async () => {
+    let url = 'https://covid19.mathdro.id/api';
     try {
         const { data: { countries } } = await axios.get(`${url}/countries`);
 
